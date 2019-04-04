0b8b17bed2343400d36ce9209f0be6d7859ba65c
Bigtimbers <therealguitarman@gmail.com>
2019-04-04 16:37:48 -0700

    smol fix

--- 1 - second-personal-project/front-end/src/components/Display.js
+++ 1 - second-personal-project/front-end/src/components/Display.js
@@ -32,9 +32,8 @@ class Display extends Component {
                                 <td>
                                     <Link button to={`/profile/${element._id}`}><h1>{element.name}</h1></Link>
                                 <td className='my-3'>
-                                    <Link className="" button to={`/profile/${element._id}`}><h1 classname=''>{element.name}</h1></Link>
+                                    <Link  button to={`/profile/${element._id}`}><h1 >{element.name}</h1></Link>
                                 </td>
-                                <td>
                                     <h5>{element.category} </h5>
                                 </td>     
                             

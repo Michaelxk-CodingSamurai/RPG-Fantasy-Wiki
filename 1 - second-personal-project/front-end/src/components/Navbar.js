7675fc57838e9488af8c4744ea323d8edd14e862
alexLarrabee <37607885+alexLarrabee@users.noreply.github.com>
2019-04-05 16:45:37 -0700

    No changes
    No CHanges

--- 1 - second-personal-project/front-end/src/components/Navbar.js
+++ 1 - second-personal-project/front-end/src/components/Navbar.js
@@ -70,7 +70,7 @@ class Navbar extends Component {
                     </ul>
 
                     <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="..." onChange = {(e) => this.setState({search: e.target.value})}/>
+                        <input className="form-control mr-sm-2" type="search" placeholder="..." value={this.state.search} onChange = {(e) => this.setState({search: e.target.value})}/>
                         <button className="btn btn-sm btn-light" onClick = {this.passSearch}><Link to="/results"> <h4 className='mt-2 text-dark'>Search</h4> </Link></button>
                     </form>
                 </div>
